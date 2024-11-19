e3fccdffeba9bb5dfec1283354629f0d0ac6872d
Jamshed <juraev@bankplanet9.com>
2024-11-19 13:45:00 +0500

    add config viper

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "github.com/JamshedJ/WalletAPI/config"
 
+func init() {
+	if err := config.InitConfig(); err != nil {
+		panic("cannot initialize config: " + err.Error())
+	}
+}
 func main() {
-	
+
 }
---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+var config *Config
+
+func Get() *Config {
+	return config
+}
+
+type Config struct {
+	App struct {
+		Port        int
+		Environment string
+		Database    struct {
+			Dsn string
+		}
+	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("config")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("cannot initialize config: %v", err)
+	}
+	viper.Unmarshal(&config)
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		if err := viper.Unmarshal(&config); err != nil {
+			log.Default().Printf("cannot unmarshal config: %v\n", err)
+		}
+	})
+
+	return nil
+}

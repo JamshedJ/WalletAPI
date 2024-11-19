b386894fc622319b2624b98efe68380336c47c04
Jamshed <juraev@bankplanet9.com>
2024-11-19 14:17:03 +0500

    init db connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,27 @@ import (
 	"github.com/JamshedJ/WalletAPI/config"
 	"github.com/JamshedJ/WalletAPI/delivery/api"
 	"github.com/JamshedJ/WalletAPI/infrastructure/glog"
+	"github.com/JamshedJ/WalletAPI/infrastructure/repository/gormRepo"
 )
+
 func init() {
 	if err := config.InitConfig(); err != nil {
 		panic("cannot initialize config: " + err.Error())
 	}
 }
+
 func main() {
 	logger := glog.NewLogger()
-	logger.Info().Msg("starting application")
+
+	err := gormRepo.InitDatabase(config.Get().App.Database.Dsn)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("cannot initialize database")
+	}
+
+	err = gormRepo.AutoMigrate()
+	if err != nil {
+		logger.Fatal().Err(err).Msg("cannot migrate database")
+	}
 
 	api.Run(nil, config.Get().App.Port)
 }
---
+++ infrastructure/repository/gormRepo/gorm.go
@@ -0,0 +1,26 @@
+package gormRepo
+
+import (
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDatabase(dsn string) error {
+	var err error
+	DB, err = gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+	}), &gorm.Config{
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+	})
+	return err
+}
+
+func AutoMigrate() error {
+	return DB.AutoMigrate()
+}
